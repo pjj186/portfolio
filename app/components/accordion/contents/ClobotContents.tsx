3db769bf2b0b557113824332f9164c56c4c31730
Lenny <66498240+pjj186@users.noreply.github.com>
2024-07-03 23:57:25 +0900

    feat(experience): 이력 부분 컨텐츠 작업
    - 이력 부분 컨텐츠 작업

--- app/components/accordion/contents/ClobotContents.tsx
+++ app/components/accordion/contents/ClobotContents.tsx
@@ -1,5 +1,52 @@
+import { Button, Tooltip } from "@nextui-org/react";
+import { FaReact } from "react-icons/fa";
+import { PiBagSimpleBold } from "react-icons/pi";
+import { RiNextjsFill } from "react-icons/ri";
+import { SiTypescript } from "react-icons/si";
+
 const ClobotContents = () => {
-  return <div>Clobot Contents</div>;
+  return (
+    <div className='py-2'>
+      <p className='text-small text-foreground/50'>
+        국내외 다양한 로봇 기술을 활용해 고객이 도입하고자 하는 로봇 HW와 실제
+        운영 환경을 고려해 로봇 특화 서비스를 제공하는 회사
+      </p>
+      <ol className='mt-2 text-sm'>
+        <li className='flex items-center gap-1'>
+          <PiBagSimpleBold />
+          사족 보행 로봇(SPOT)의 관제 서비스 UI / UX 개발
+        </li>
+      </ol>
+      <div className='mt-2 flex gap-3'>
+        <Button size='sm' variant='flat' color='primary'>
+          배운점
+        </Button>
+        <Button size='sm' variant='flat' color='primary'>
+          아쉬웠던점
+        </Button>
+      </div>
+      <p className='mt-2 text-small text-foreground/50'>
+        <strong>What I Used</strong>
+      </p>
+      <div className='mt-1 flex gap-2'>
+        <Tooltip content='React' placement='bottom'>
+          <Button isIconOnly variant='bordered'>
+            <FaReact size={25} />
+          </Button>
+        </Tooltip>
+        <Tooltip content='TypeScript' placement='bottom'>
+          <Button isIconOnly variant='bordered'>
+            <SiTypescript size={25} />
+          </Button>
+        </Tooltip>
+        <Tooltip content='Nextjs' placement='bottom'>
+          <Button isIconOnly variant='bordered'>
+            <RiNextjsFill size={25} />
+          </Button>
+        </Tooltip>
+      </div>
+    </div>
+  );
 };
 
 export default ClobotContents;
--- app/components/accordion/contents/MetalabContents.tsx
+++ app/components/accordion/contents/MetalabContents.tsx
@@ -1,5 +1,72 @@
+import { PiBagSimpleBold } from "react-icons/pi";
+import { FaReact } from "react-icons/fa";
+import { SiTypescript } from "react-icons/si";
+import { Button, Tooltip } from "@nextui-org/react";
+import { RiNextjsFill } from "react-icons/ri";
+import { SiPrisma } from "react-icons/si";
+import { SiPlanetscale } from "react-icons/si";
+import { FaAws } from "react-icons/fa";
+
 const MetalabContents = () => {
-  return <div>Contents of Metalab</div>;
+  return (
+    <div className='py-2'>
+      <p className='text-small text-foreground/50'>
+        외주용역 / 국가지원사업 아웃소싱을 주로 진행했던 스타트업 회사
+      </p>
+      <ol className='mt-2 text-sm'>
+        <li className='flex items-center gap-1'>
+          <PiBagSimpleBold />첫 번째 개발자로 입사하여 개발 / 배포 / 운영 담당
+        </li>
+        <li className='flex items-center gap-1'>
+          <PiBagSimpleBold />
+          다른 업체에서 프로젝트를 받아서 수정 및 유지보수 / 재배포
+        </li>
+      </ol>
+      <div className='mt-2 flex gap-3'>
+        <Button size='sm' variant='flat' color='primary'>
+          배운점
+        </Button>
+        <Button size='sm' variant='flat' color='primary'>
+          아쉬웠던점
+        </Button>
+      </div>
+      <p className='mt-2 text-small text-foreground/50'>
+        <strong>What I Used</strong>
+      </p>
+      <div className='mt-1 flex gap-2'>
+        <Tooltip content='React' placement='bottom'>
+          <Button isIconOnly variant='bordered'>
+            <FaReact size={25} />
+          </Button>
+        </Tooltip>
+        <Tooltip content='TypeScript' placement='bottom'>
+          <Button isIconOnly variant='bordered'>
+            <SiTypescript size={25} />
+          </Button>
+        </Tooltip>
+        <Tooltip content='Nextjs' placement='bottom'>
+          <Button isIconOnly variant='bordered'>
+            <RiNextjsFill size={25} />
+          </Button>
+        </Tooltip>
+        <Tooltip content='Prisma' placement='bottom'>
+          <Button isIconOnly variant='bordered'>
+            <SiPrisma size={25} />
+          </Button>
+        </Tooltip>
+        <Tooltip content='Planet Scale' placement='bottom'>
+          <Button isIconOnly variant='bordered'>
+            <SiPlanetscale size={25} />
+          </Button>
+        </Tooltip>
+        <Tooltip content='AWS EC2' placement='bottom'>
+          <Button isIconOnly variant='bordered'>
+            <FaAws size={25} />
+          </Button>
+        </Tooltip>
+      </div>
+    </div>
+  );
 };
 
 export default MetalabContents;
